a679cc75776b05901e06418cdc40ae7d559062e9
santosh <santosh.ece06@gmail.com>
2018-12-13 18:10:46 +0530

    Multiple Route.

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import {HomeScreen} from './src/HomeScreen';
+import {HomeScreen, DetailScreen} from './src';
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Details: DetailScreen
+  }, {
+    initialRouteName: "Details"
   }
 );
 
